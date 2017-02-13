d071dcf6ec98c88b1e7581228efc3d73a4407d18
Milena <Milena@Milena-PC.home>
2017-02-13 19:13:39 +0100

    losowanie

--- Lesson1/LessonTask1/src/Main.java
+++ Lesson1/LessonTask1/src/Main.java
@@ -1,11 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		System.out.println("hello world Milena");
+		Random generator = new Random();
+		Scanner scan = new Scanner(System.in);
+
+		int userNumber;
+		int generatedNumber;
+		System.out.println("Wprowadz liczbe: ");
+		userNumber = scan.nextInt();
+
+		int numberOfTry = 0;
+		do {
+			numberOfTry++;
+			generatedNumber = generator.nextInt(30);
+			System.out.println("Wylosowales liczbe " + generatedNumber + " w losowaniu " + numberOfTry);
+		} while (userNumber != generatedNumber);
 		
+
 	}
 
 }
