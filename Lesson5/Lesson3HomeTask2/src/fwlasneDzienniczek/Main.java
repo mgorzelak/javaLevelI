c64513fff6949c78ae37e1563a25d6b6d022188b
Milena <Milena@Milena-PC>
2017-04-01 15:43:22 +0200

    zadanie home 2/ lesson 3 - funkcje wlasne dzienniczek

---
+++ Lesson5/Lesson3HomeTask2/src/fwlasneDzienniczek/Main.java
@@ -0,0 +1,72 @@
+package fwlasneDzienniczek;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("dzienniczek ucznia");
+
+		System.out.println("Podaj imie: ");
+		String name = getScanner().next();
+
+		System.out.println("Podaj nazwisko: ");
+		String surname = getScanner().next();
+
+		System.out.println("Podaj nazwe szkoly: ");
+		String school = getScanner().nextLine();
+
+		System.out.println("wybierz poziom edukacji");
+		System.out.println("1.Podstawowka");
+		System.out.println("2.Gimnazjum");
+
+		int level = getScanner().nextInt();
+		switch (level) {
+
+		case 1:
+			System.out.println("Podstawowka");
+
+			break;
+		case 2:
+			System.out.println("Gimnazjum");
+
+			break;
+		default:
+			System.out.println("brak tej pozycji w menu");
+			break;
+		}
+		System.out.println("Podaj nr klasy (1-6 w szko³ach podstawowych, 1-3 w gimnazjach): ");
+		int numberOfClass = getScanner().nextInt();
+
+		System.out.println("Podaj profil klasy (np. matematyczna, biologiczna, ogólna itd.)");
+		String profil = getScanner().next();
+
+		System.out.println("imie i nazwisko ucznia " + name + " " + surname);
+		System.out.println("nazwa szkoly " + school);
+		System.out.println("klasa  " + numberOfClass);
+		System.out.println("profil " + profil);
+		if (level == 1) {
+			System.out.println("przedmioty: WF, Jêzyk Polski, Matematyka ");
+
+		} else {
+			System.out.println("przedmioty: WF, Jêzyk Polski, Matematyka, Chemia i Fizyka ");
+		}
+		System.out.println("czy uczen uczeszcza na dodatkowe zajecia? Wpisz true/false");
+
+		if (getScanner().nextBoolean() == true) {
+			System.out.println("Podaj dodatkowe kursy: ");
+			String extraLessons = scanner.nextLine();
+			System.out.println("Dodatkowe kursy to: " + extraLessons);
+		}
+	}
+
+	public static Scanner getScanner() {
+		if (scanner != null) {
+			scanner = new Scanner(System.in);
+		}
+		return scanner;
+	}
+
+}
